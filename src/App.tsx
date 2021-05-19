3bc2de664584a6b3f9b8f2a58a9cd0bb6d5bf2a1
Pankaj <pmeghani@purdue.edu>
2021-05-19 16:27:18 -0400

    Using functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,79 @@
 
 import * as React from 'react';
 import { hot } from "react-hot-loader/root";
+import { Post, Comment, usePostQuery, POSTS_QUERY } from "./Post"
 
 interface Props {
-   name:
-    string
+  name: string
 }
 
-class App extends React.Component<Props> {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>
-          Hello {name}
-        </h1>
-      </>
-    );
-  }
+interface State {
+  text: string
 }
+
+
+const App = () => {
+  // const [text, updateText] = React.useState("")
+
+  const { loading, error, data } = usePostQuery(POSTS_QUERY)
+
+  console.log(data)
+
+  if (loading) return <h1>{"Loading..."}</h1>
+
+  // if (error) return <h1>{"Some error occured"}</h1>
+
+  // if (data && data[0]) return <h1>{data[0].title}</h1>
+
+  return (
+    <>
+      {(error || data == undefined) ? (
+        <h1>{"Standby"}</h1>
+      ) : (
+        data && data.posts.map((value: Post) => <h1>{value.title}</h1>)
+      )}
+    </>)
+
+}
+
+// class App extends React.Component<Props, State> {
+
+//   constructor(props: Props) {
+//     super(props)
+//     this.state = { text: "" }
+//     this.componentDidMount = this.componentDidMount.bind(this)
+//   }
+
+//   async componentDidMount() {
+//     // let response: Response = await fetch("https://mockend.com/ParadoxicalNerd/bugtracker-fronend/posts/2")
+//     // let data: Post = await response.json()
+//     // console.log(data)
+//     // this.setState(prevstate => {
+//     //   return {
+//     //     ...prevstate,
+//     //     text: data.title
+//     //   }
+//     // })
+
+//     const { loading, error, data } = usePostQuery(POSTS_QUERY)
+
+//     if (loading) this.setState({ text: "Loading..." })
+//     if (error) this.setState({ text: "Unexpected error occured" })
+
+//     if (data) this.setState({ text: data[0].title })
+
+//   }
+
+//   render() {
+//     const { name } = this.props;
+//     return (
+//       <>
+//         <h1>
+//           Hello {this.state.text}
+//         </h1>
+//       </>
+//     );
+//   }
+// }
 
 export default hot(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
+import { ApolloClient, ApolloProvider, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
 import * as React from 'react';
 import * as ReactDOM from "react-dom";
 
 import App from './App';
 import "./styles.scss";
 
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+const client = new ApolloClient({
+    uri: `https://mockend.com/ParadoxicalNerd/bugtracker-fronend/graphql`,
+    cache: new InMemoryCache()
+})
+
+let mountNode = document.getElementById("app");
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    mountNode
+);
