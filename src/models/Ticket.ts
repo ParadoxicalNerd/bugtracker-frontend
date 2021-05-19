65306b971be8b5808dc49aab28509d0b87b297ba
Pankaj <pmeghani@purdue.edu>
2021-05-19 16:53:30 -0400

    Trying enum with mockend

--- src/models/Ticket.ts
+++ src/models/Ticket.ts
@@ -1,9 +1,9 @@
 import { Project } from "./Project";
 import { User } from "./User";
 
-export enum ticketTypes { BUG, FEATURE, DOCS }
-export enum ticketStatus { OPEN, ASSIGNED, TESTING, RESOLVED }
-export enum ticketPriority { UNKNOWN, LOW, MEDIUM, HIGH, CRITICAL }
+export enum ticketTypes { BUG = 'BUG', FEATURE = 'FEATURE', DOCS = 'DOCS' }
+export enum ticketStatus { OPEN = 'OPEN', ASSIGNED = 'ASSIGNED', TESTING = 'TESTING', RESOLVED = 'RESOLVED' }
+export enum ticketPriority { UNKNOWN = 'UNKNOWN', LOW = 'LOW', MEDIUM = 'MEDIUM', HIGH = 'HIGH', CRITICAL = 'CRITICAL' }
 
 export interface Ticket {
     title: string,
