e65bcc2227b86591d4b549dde844ed2163222bda
Pankaj <meghanipankaj5@gmail.com>
2021-08-02 16:44:49 -0400

    Removed env

--- src/views/TicketDisplayView.tsx
+++ src/views/TicketDisplayView.tsx
@@ -1,121 +1,141 @@
-import * as React from "react"
-import { Card, InputGroup, Row, Col, Container, Button, Table, FormControl, CardGroup } from "react-bootstrap"
-import { Link, useRouteMatch } from "react-router-dom"
+import * as React from "react";
+import {
+    Button,
+    Card,
+    CardGroup,
+    Col,
+    Container,
+    FormControl,
+    InputGroup,
+    Row,
+    Table,
+} from "react-bootstrap";
+import { Link, useRouteMatch } from "react-router-dom";
 import { Ticket } from "../models";
 
 const ticketDetails = (ticket: Ticket) => {
-    let { url } = useRouteMatch()
+    let { url } = useRouteMatch();
 
-    return (<Card>
-        <Card.Body>
-            <Row>
-                <Col><Card.Title><h3>Details for ticket #{ticket.id}</h3></Card.Title></Col>
-                <Col md="auto">
-                    <Button size="sm">
-                        <Link to={`${url}?edit="true"`} className="btn btn-primary btn-sm">Edit</Link>
-                    </Button>
-                </Col>
-            </Row>
-            <Container>
+    return (
+        <Card>
+            <Card.Body>
                 <Row>
                     <Col>
-                        <Card border="light">
-                            <Card.Body>
-                                <Card.Title>Ticket Title</Card.Title>
-                                <Card.Text>{ticket.title}</Card.Text>
-                            </Card.Body>
-                        </Card>
+                        <Card.Title>
+                            <h3>Details for ticket #{ticket.id}</h3>
+                        </Card.Title>
                     </Col>
-                    <Col>
-                        <Card border="light">
-                            <Card.Body>
-                                <Card.Title>Ticket Description</Card.Title>
-                                <Card.Text>{ticket.description}</Card.Text>
-                            </Card.Body>
-                        </Card>
+                    <Col md="auto">
+                        <Button size="sm">
+                            <Link to={`${url}?edit="true"`} className="btn btn-primary btn-sm">
+                                Edit
+                            </Link>
+                        </Button>
                     </Col>
                 </Row>
-                <Row>
-                    <Col>
-                        <Card border="light">
-                            <Card.Body>
-                                <Card.Title>Assigned Developer</Card.Title>
-                                <Card.Text>{ticket.assignedTo?.name}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card border="light">
-                            <Card.Body>
-                                <Card.Title>Submitter</Card.Title>
-                                <Card.Text>{ticket.author.name}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Card border="light">
-                            <Card.Body>
-                                <Card.Title>Project</Card.Title>
-                                <Card.Text>{ticket.project.name}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card border="light">
-                            <Card.Body>
-                                <Card.Title>Ticket Priority</Card.Title>
-                                <Card.Text>{ticket.priority}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Card border="light">
-                            <Card.Body>
-                                <Card.Title>Ticket Status</Card.Title>
-                                <Card.Text>{ticket.status}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card border="light">
-                            <Card.Body>
-                                <Card.Title>Ticket Type</Card.Title>
-                                <Card.Text>{ticket.type}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Card border="light">
-                            <Card.Body>
-                                <Card.Title>Created</Card.Title>
-                                <Card.Text>{ticket.creationDate}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card border="light">
-                            <Card.Body>
-                                <Card.Title>Updated</Card.Title>
-                                <Card.Text>{ticket.updateDate}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </Card.Body>
-    </Card>)
-}
+                <Container>
+                    <Row>
+                        <Col>
+                            <Card border="light">
+                                <Card.Body>
+                                    <Card.Title>Ticket Title</Card.Title>
+                                    <Card.Text>{ticket.title}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card border="light">
+                                <Card.Body>
+                                    <Card.Title>Ticket Description</Card.Title>
+                                    <Card.Text>{ticket.description}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Card border="light">
+                                <Card.Body>
+                                    <Card.Title>Assigned Developer</Card.Title>
+                                    <Card.Text>{ticket.assignedTo?.name}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card border="light">
+                                <Card.Body>
+                                    <Card.Title>Submitter</Card.Title>
+                                    <Card.Text>{ticket.author.name}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Card border="light">
+                                <Card.Body>
+                                    <Card.Title>Project</Card.Title>
+                                    <Card.Text>{ticket.project.name}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card border="light">
+                                <Card.Body>
+                                    <Card.Title>Ticket Priority</Card.Title>
+                                    <Card.Text>{ticket.priority}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Card border="light">
+                                <Card.Body>
+                                    <Card.Title>Ticket Status</Card.Title>
+                                    <Card.Text>{ticket.status}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card border="light">
+                                <Card.Body>
+                                    <Card.Title>Ticket Type</Card.Title>
+                                    <Card.Text>{ticket.type}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Card border="light">
+                                <Card.Body>
+                                    <Card.Title>Created</Card.Title>
+                                    <Card.Text>{ticket.creationDate}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card border="light">
+                                <Card.Body>
+                                    <Card.Title>Updated</Card.Title>
+                                    <Card.Text>{ticket.updateDate}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </Card.Body>
+        </Card>
+    );
+};
 
-const ticketComments = (ticket: Ticket) => (
+const ticketComments = (ticket: Ticket, comment: string, onChange: any, onSubmit: any) => (
     <Card>
         <Card.Body>
-            <Card.Title><h3>Ticket Comments</h3></Card.Title>
+            <Card.Title>
+                <h3>Ticket Comments</h3>
+            </Card.Title>
             <Card.Subtitle className="mb-2 text-muted">All comments for this ticket</Card.Subtitle>
             <Table striped hover responsive>
                 <thead>
@@ -126,34 +146,53 @@ const ticketComments = (ticket: Ticket) => (
                     </tr>
                 </thead>
                 <tbody>
-                    {ticket.comments ? ticket.comments.map((comment) => (
-                        comment ? <tr key={comment.id}>
-                            <td>{comment.author.name}</td>
-                            <td>{comment.message}</td>
-                            <td>{comment.creationDate}</td>
-                        </tr> : null
-                    )) : null}
+                    {ticket.comments
+                        ? ticket.comments.map((comment) =>
+                              comment ? (
+                                  <tr key={comment.id}>
+                                      <td>{comment.author.name}</td>
+                                      <td>{comment.message}</td>
+                                      <td>{comment.creationDate}</td>
+                                  </tr>
+                              ) : null
+                          )
+                        : null}
                 </tbody>
             </Table>
             <InputGroup>
                 <FormControl
                     placeholder="Input Comment"
                     aria-label="Input Comment"
+                    value={comment}
+                    onChange={onChange}
                 />
                 <InputGroup.Append>
-                    <Button>Submit</Button>
+                    <Button onClick={onSubmit}>Submit</Button>
                     {/* TODO: Implement actual functionality */}
                 </InputGroup.Append>
             </InputGroup>
-        </Card.Body >
-    </Card >
-)
+        </Card.Body>
+    </Card>
+);
 
-export default ({ ticket }: { ticket: Ticket }) => (
-    <Container>
-        <CardGroup>
-            {ticketDetails(ticket)}
-            {ticketComments(ticket)}
-        </CardGroup>
-    </Container>
-)
+export default ({ ticket }: { ticket: Ticket }) => {
+    const [comment, setComment] = React.useState("");
+
+    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+        const target: any = event.target;
+        setComment((val) => target.value);
+    };
+
+    const onSubmit = (event: React.FormEvent<HTMLInputElement>) => {
+        event.preventDefault();
+    };
+
+    return (
+        <Container>
+            <CardGroup>
+                {ticketDetails(ticket)}
+                {ticketComments(ticket, comment, onChange, onSubmit)}
+            </CardGroup>
+        </Container>
+    );
+};
--- src/views/TicketView.tsx
+++ src/views/TicketView.tsx
@@ -1,97 +1,11 @@
+import { parse } from "query-string";
 import * as React from "react";
-import { parse, stringify } from "query-string";
-import {
-    Button,
-    Card,
-    CardGroup,
-    Col,
-    Container,
-    Form,
-    FormControl,
-    FormGroup,
-    InputGroup,
-    Row,
-    Table,
-} from "react-bootstrap";
-import { useHistory, useLocation, useParams, useRouteMatch, Switch, Route } from "react-router";
 import { Spinner } from "react-bootstrap";
+import { useLocation, useParams } from "react-router";
 import ticketAccessor from "../controllers/TicketAccessor";
 import usernamesAccessor from "../controllers/UsersAccessor";
-import TicketEditorView from "./TicketEditorView";
 import TicketDisplayView from "./TicketDisplayView";
-import _TicketEditorView from "./TicketEditorView";
-
-// const ticketPriorityDropdownRef = React.useRef<FormGroup | null>(null)
-
-/*
-const TicketView = () => {
-
-    const params = useParams<{ ticketID: string }>();
-
-    // let params = parse(search, { parseNumbers: true, parseBooleans: true })
-    // let history = useHistory()
-
-    // const [editTicket, modifyEditTicket] = React.useState(false)
-
-
-    // React.useEffect(() => {
-    //     if (editTicket) {
-    //         parsed_object["edit"] = true
-    //     } else {
-    //         parsed_object
-    //     }
-
-    //     const newLocation = `#${pathname}?${stringify(parsed_object)}`
-
-    //     console.log(pathname, '->', newLocation)
-
-    //     window.history.replaceState({}, '', newLocation);
-    // })
-
-    // React.useEffect(() => {
-    //     console.log("hooker", search)
-    //     let parsed_object = parse(search)
-
-    //     // debugger
-    // })
-
-
-    const { data: ticket_data, error: ticket_error, fetching: ticket_loading, } = ticketAccessor(params.ticketID)
-    const { data: usernames_data, error: usernames_error, fetching: usernames_loading } = usernamesAccessor()
-
-    // console.log(ticket_data)
-
-    if (ticket_loading || usernames_loading) return <Spinner animation="border" variant="primary" />
-
-    if (ticket_error || ticket_data == undefined || usernames_error || usernames_data == undefined) {
-        return <h1>Unexpected Error</h1>
-    }
-
-    allUsers = usernames_data.allUsers
-
-
-    // Conditional rendoring based on whether the user wants to edit the ticket or not
-    const { path } = useRouteMatch()
-    let { search, pathname } = useLocation()
-
-    console.log(path, pathname, search)
-
-    const search_parameter = parse(search, { parseNumbers: true, parseBooleans: true })
-    console.log(search_parameter)
-
-    // <Switch>
-    //     <Route path={`${path}?edit="true"`} exact>
-    //         <TicketEditorView ticket={ticket_data.ticket} allUsers={allUsers} />
-    //     </Route>
-    //     <Route path={`${path}`}>
-    //         <TicketDisplayView ticket={ticket_data.ticket} />
-    //     </Route>
-    // </Switch>
-    if (search_parameter.edit == undefined && search_parameter.edit != true) return <TicketDisplayView ticket={ticket_data.ticket} />
-    else return <TicketEditorView ticket={ticket_data.ticket} allUsers={allUsers} />
-
-}
-*/
+import TicketEditorView from "./TicketEditorView";
 
 const TicketView = () => {
     const params = useParams<{ ticketID: string }>();
@@ -127,8 +41,7 @@ const TicketView = () => {
 
     if (search_parameter.edit == undefined && search_parameter.edit != true)
         return <TicketDisplayView ticket={ticket_data.ticket} />;
-    else
-        return <_TicketEditorView ticket={ticket_data.ticket} allUsers={usernames_data.allUsers} />;
+    else return <TicketEditorView ticket={ticket_data.ticket} allUsers={usernames_data.allUsers} />;
 };
 
 export default TicketView;
